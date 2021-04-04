35e5877494ecf7665c9a9aa9f1763518353be25e
Dima <fildyaev@mail.ru>
2021-04-04 15:05:29 +0300

    Практика 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDb = 
+{
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  generes: [],
+  privat: false
+
+};
+
+let a = prompt("Один из последних просмотренных фильмов?", "");
+let b = +prompt("На сколько оцените его?", "");
+let c = prompt("Один из последних просмотренных фильмов?", "");
+let d = +prompt("На сколько оцените его?", "");
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log (personalMovieDb);
+
+
+
 
 
 
